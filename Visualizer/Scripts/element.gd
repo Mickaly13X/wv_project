394e00e2741ce2b8201ba77a257e3fcd9a59d74b
Yong Wook Kim <eric.mickaly13x@hotmail.com>
2021-02-24 12:54:38 +0100

    FGP Performed: This is a fast git push, minor changes have been made and I didn't feel the need to commit a message.

--- Visualizer/Scripts/element.gd
+++ Visualizer/Scripts/element.gd
@@ -5,7 +5,7 @@ var shape = StyleBoxFlat.new()
 func _ready():
 	
 	shape.set_corner_radius_all(8)
-	shape.bg_color = Color.red
+	shape.bg_color = Color(0.335114, 0.295376, 0.613281)
 
 func _draw():
 	draw_style_box(shape, Rect2(0, 0, 16, 16))

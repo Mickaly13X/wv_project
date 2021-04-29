73186ffb460bf9bca57cc5c40926dc71bfcc5a16
Yong Wook Kim <eric.mickaly13x@hotmail.com>
2021-04-29 22:46:28 +0200

    doc formula update and tables for sterling number

--- Visualizer/Scripts/doc_tab_panel.gd
+++ Visualizer/Scripts/doc_tab_panel.gd
@@ -9,17 +9,30 @@ var TITLES = [
 	"partition of n into x subsets:"
 ]
 
+onready var FORMULAS = [
+	load("Textures/sequence.png"),
+	load("Textures/permutation.png"),
+	load("Textures/subset.png"),
+	load("Textures/multisubset.png"),
+	load("Textures/composition.png"),
+	load("Textures/partition.png")
+	
+]
+
 export var index = 0
 var title
 
 
 func _ready():
-	set_image_index(index)
-	set_title(TITLES[index])
+	set_index(index)
 
 func set_image_index(idx : int) -> void:
 	
-	$VBox/Formula/Image.frame = idx
+	#$VBox/Formula.set_texture(FORMULAS[idx])
+	$VBox/VBox/Formula/Image.frame = idx
+	var sep = $VBox/VBox/Formula/Image.frames.get_frame("default",idx).get_height()/2 +32
+	$VBox/VBox.set("custom_constants/separation", sep)
+	
 
 
 func set_title(title : String) -> void:
@@ -33,6 +46,10 @@ func set_index(idx : int) -> void:
 	self.index = idx
 	set_image_index(idx)
 	set_title(TITLES[idx])
+	if idx == 4 || idx == 5:
+		$VBox/Table.show()
+	else:
+		$VBox/Table.hide()
 	
 
 func set_content(content : String) -> void:
